b7bba0290844d0c780b572dea680c5cc876858b8
Medet Tleukabiluly <m.diddy_11@mail.ru>
2021-08-31 20:23:59 +0600

    use icon

--- template/src/screens/home.tsx
+++ template/src/screens/home.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { StackScreenProps } from '@react-navigation/stack'
 import { StyleSheet, Text, View, Button } from 'react-native'
+import { StackScreenProps } from '@react-navigation/stack'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 import { RootStackParamList } from 'src/types'
 
@@ -10,6 +11,7 @@ export default function Screen ({
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Hello</Text>
+      <Ionicons name='globe' />
       <Button title='About' onPress={() => navigation.navigate('About')} />
     </View>
   )
