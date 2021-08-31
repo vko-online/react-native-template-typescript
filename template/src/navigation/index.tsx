990800586bbcc465d16814c42f7389f58195edef
Medet Tleukabiluly <m.diddy_11@mail.ru>
2021-08-31 20:14:11 +0600

    add about screen

--- template/src/navigation/index.tsx
+++ template/src/navigation/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import {
-  NavigationContainer
-} from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 
 import HomeScreen from 'src/screens/home'
+import AboutScreen from 'src/screens/about'
 import { RootStackParamList } from 'src/types'
 
 export default function Navigation() {
@@ -19,9 +18,9 @@ const Stack = createStackNavigator<RootStackParamList>()
 
 function RootNavigator() {
   return (
-    <Stack.Navigator
-      screenOptions={{headerShown: false, animationEnabled: true}}>
+    <Stack.Navigator>
       <Stack.Screen name='Home' component={HomeScreen} />
+      <Stack.Screen name='About' component={AboutScreen} />
     </Stack.Navigator>
   )
 }
--- template/src/screens/about.tsx
+++ template/src/screens/about.tsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import { StackScreenProps } from '@react-navigation/stack'
-import { StyleSheet, Text, View, Button } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 
 import { RootStackParamList } from 'src/types'
 
 export default function Screen ({
   navigation
-}: StackScreenProps<RootStackParamList, 'Home'>) {
+}: StackScreenProps<RootStackParamList, 'About'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello</Text>
-      <Button title='About' onPress={() => navigation.navigate('About')} />
+      <Text style={styles.title}>About</Text>
     </View>
   )
 }
